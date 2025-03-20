7b2cb6e09e6b5f0f92cc97f8f86d4df9f60c74c7
ravi688 <rp0412204@gmail.com>
2025-03-20 23:52:54 +0000

    install fixes: run pyinstaller as non-sudo, and also pass --clean flag to it

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ if ! pip show certifi > /dev/null 2>&1; then
 fi
 
 # Package meson into one executable
-(pyinstaller --onefile --runtime-hook=runtime_hook.py --add-data "$(python -m certifi):certifi" --add-data "mesonbuild:mesonbuild" meson.py)
+sudo -u $SUDO_USER pyinstaller --onefile --clean --runtime-hook=runtime_hook.py --add-data "$(python -m certifi):certifi" --add-data "mesonbuild:mesonbuild" meson.py
 
 if [ -z $INSTALL_PREFIX ]; then
 	INSTALL_PREFIX="/usr"
